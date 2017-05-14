13111f4b0d5c7dab1a4f0c75f33500180ac59eb8
Arslan Yaqoob <bsef15m532@pucit.edu.pk>
2017-05-14 09:35:01 -0700

    Update FCFS.py

--- FCFS.py
+++ FCFS.py
@@ -1,19 +1,34 @@
-#!/usr/bin/python
-a=[]
-t=0
-total=raw_input("Enter total number of processes:")
+def initialize(process,total_process):
+	index=0	
+	for index in xrange(int(total_process)):
+		process.append([])
+		process[index].append(raw_input("Enter Name of process:"))
+		process[index].append(int(raw_input("Arrival Time:")))
+		process[index].append(int(raw_input("burst time:")))
 
-for i in xrange(int(total)):
-	a.append([])
-	a[i].append(raw_input("Enter Name of process:"))
-	a[i].append(int(raw_input("Arrival Time:")))
-	a[i].append(int(raw_input("burst time:")))
-	t=t+a[i][2]
+	
+def print_process(process,total_process):
+	total_burst=0
+	print 'PName\tA. Time\tB.Time'
+	index=0
+	for index in xrange(int(total_process)):
+        	print process[index][0] ,'\t',process[index][1],'\t',process[index][2]
+        	total_burst+=process[index][2]
+	
+	print "Total Time : ",total_burst
 
-a.sort(key= lambda a:a[1])
-print 'PName\tA. Time\tB.Time'
 
-for i in xrange(int(total)):
-	print a[i][0] ,'\t',a[i][1],'\t',a[i][2]
+def sorting(process):
+	process.sort(key=lambda process:process[1])
 
-print "Total Time : ",t
+
+
+
+process=[]
+total_process=0
+total_process=raw_input("Enter total number of processes:")
+initialize(process,total_process)
+sorting(process)
+print_process(process,total_process)
+
+
